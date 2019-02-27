85bab9f721018a7f40bce4f98edfe8ba794daac6
Víctor Navarro González <victor.navarro@enti.cat>
2019-02-27 17:09:24 +0100

    Simplified interfaces

--- Delegate.h
+++ Delegate.h
@@ -10,50 +10,55 @@
 #include <algorithm>
 #include <type_traits>
 
+// Helper to get the address of a std::function.
+template<typename T, typename... U>
+static size_t getAddress(std::function<T(U...)> f)
+{
+	typedef T(fnType)(U...);
+	auto fnPointer = f.template target<fnType *>();
+	return (size_t) *fnPointer;
+}
+
+template<typename T>
+struct function_traits;
+
+template<typename R, typename ...Args>
+struct function_traits<std::function<R(Args...)>>
+{
+	static const size_t nargs = sizeof...(Args);
+
+	typedef R result_type;
+
+	template<size_t i>
+	struct arg
+	{
+		typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+	};
+};
+
 template<class T>
 class Delegate
 {
-private:
+protected:
 	using Function = std::function<T>;
 	std::vector<Function> functions;
 
 	using ResultType = typename Function::result_type;
 	std::vector<ResultType> results;
 
-	// Helper to get the address of a std::function.
-	template<typename T, typename... U>
-	static size_t getAddress(std::function<T(U...)> f)
-	{
-		typedef T(fnType)(U...);
-		auto fnPointer = f.template target<fnType *>();
-		return (size_t) *fnPointer;
-	}
-
 public:
 	Delegate() = default;
 
 	~Delegate() = default;
 
-	auto Invoke()
+	template<typename ... Params>
+	auto Invoke(Params ... params)
 	{
 		results.clear();
 
 		for (const auto &function : functions)
 		{
-			results.push_back(function());
-		}
-
-		return results;
-	}
-
-	template<class ... Args>
-	auto Invoke(Args ... args)
-	{
-		results.clear();
-
-		for (const auto &function : functions)
-		{
-			results.push_back(function(args...));
+			results.push_back(function(params...));
 		}
 
 		return results;
@@ -62,6 +67,56 @@ public:
 	void operator+=(const Function &rhs)
 	{
 		functions.push_back(rhs);
+	}
+
+	void operator+=(const std::vector<Function> &rhs)
+	{
+		functions.insert(functions.end(), rhs.begin(), rhs.end());
+	}
+
+	void operator-=(const Function &rhs)
+	{
+		auto found = std::find_if(functions.begin(), functions.end(),
+								  [rhs](auto function) { return getAddress(function) == getAddress(rhs); });
+
+		if (found != functions.end()) functions.erase(found);
+	}
+
+	void Clear()
+	{
+		results.clear();
+	}
+};
+
+template<class ... Args>
+class Delegate<void(Args...)>
+{
+protected:
+	using Function = std::function<void(Args...)>;
+	std::vector<Function> functions;
+
+
+public:
+	Delegate() = default;
+
+	~Delegate() = default;
+
+	void Invoke(Args ... args)
+	{
+		for (const auto &function : functions)
+		{
+			function(args...);
+		}
+	}
+
+	void operator+=(const Function &rhs)
+	{
+		functions.push_back(rhs);
+	}
+
+	void operator+=(const std::vector<Function> &rhs)
+	{
+		functions.insert(functions.end(), rhs.begin(), rhs.end());
 	}
 
 	void operator-=(const Function &rhs)
--- Func.h
+++ Func.h
@@ -8,44 +8,8 @@
 #include "Delegate.h"
 
 template<typename ... Args>
-class Func
+class Func : public Delegate<void(Args...)>
 {
-private:
-	using Function = std::function<void(Args...)>;
-	std::vector<Function> functions;
-
-	// Helper to get the address of a std::function.
-	template<typename T, typename... U>
-	static size_t getAddress(std::function<T(U...)> f)
-	{
-		typedef T(fnType)(U...);
-		auto fnPointer = f.template target<fnType *>();
-		return (size_t) *fnPointer;
-	}
-
-public:
-	Func() = default;
-
-	~Func() = default;
-
-	void Invoke(Args ... args)
-	{
-		for (const auto &function : functions) function(args...);
-	}
-
-	void operator+=(const Function &rhs)
-	{
-		functions.push_back(rhs);
-	}
-
-	void operator-=(const Function &rhs)
-	{
-		auto found = std::find_if(functions.begin(), functions.end(),
-								  [rhs](auto function) { return getAddress(function) == getAddress(rhs); });
-
-		if (found != functions.end()) functions.erase(found);
-	}
-
 };
 
 #endif
